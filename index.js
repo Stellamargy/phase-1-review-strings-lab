ddb493fdc451f2a59463daeeb8a1b2de90c21bd9
Stellamargy <stella.oloo@student.moringaschool.com>
2022-11-22 01:57:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+const currentUser = 'Stella Margy' ;
+//const welcomeMessage = 'Welcome to Flatbook' ;
+//const welcomeMessage = currentUser ;
+//const welcomeMessage = 'Welcome to Flatbook, currentUser';
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+//const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+//const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//shortGreeting
+//const shortGreeting = '';
+//const shortGreeting = 'Welcome, ';
+//const shortGreeting = `Welcome, ${currentUser}`;
+'Edsger Dijkstra'.charAt(0);
+currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
+
